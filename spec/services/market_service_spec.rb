f91d2a14907fd7dfc61009cef06840ef898df2f7
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-04-04 19:13:28 -0500

    create/test market service

---
+++ spec/services/market_service_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe MarketService do
+  it "returns list of all markets" do
+    json_response = File.read("spec/fixtures/markets_list.json")
+    
+    stub_request(:get, "http://localhost:3000/api/v0/markets")
+      .to_return(status: 200, body: json_response)
+
+    markets = MarketService.get_all_markets
+
+    expect(markets).to be_a(Hash)
+    expect(markets[:data]).to be_an(Array)
+    expect(markets[:data].size).to eq(5)
+  end
+end
